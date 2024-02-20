2b33c01a245cc02b563aabead7faae17129efa59
Anna Tsepilova <virgo.tsav@gmail.com>
2024-02-20 18:31:18 +0200

    add searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,33 @@
-export const App = () => {
-  return <></>;
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import Section from './Section/Section';
+import Searchbar from './Searchbar/Searchbar';
+class App extends Component {
+  state = {
+    searchQuery: '',
+    page: 1,
+  };
+
+  handleFormSubmit = searchQuery => {
+    let page = this.state.page;
+    if (this.state.searchQuery !== searchQuery) {
+      page = 1;
+    }
+    this.setState({
+      searchQuery: searchQuery,
+      page: page,
+    });
+  };
+
+  render() {
+    return (
+      <Section>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ToastContainer />
+      </Section>
+    );
+  }
+}
 
 export default App;
